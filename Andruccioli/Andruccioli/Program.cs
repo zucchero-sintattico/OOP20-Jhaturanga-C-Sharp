bea6ac941e1030b58d5bc912bd13da3f175c3b1d
Manuel Andruccioli <manuandru987@gmail.com>
2021-04-20 22:57:31 +0200

    Initial commit

---
+++ Andruccioli/Andruccioli/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OOP20;
+using OOP20.user;
+
+namespace Andruccioli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
