385d10739ddf1c67d8714d62eeed8134e2c30d8b
Manuel Andruccioli <manuandru987@gmail.com>
2021-04-21 19:59:59 +0200

    Add UserStorage

---
+++ Andruccioli/Andruccioli/src/userstorage/UserDataStorageInRAMStrategy.cs
@@ -0,0 +1,27 @@
+﻿using OOP20.user;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP20.userstorage
+{
+    /// <summary>
+    /// This implementation does not save data in the persistent memory,
+    /// it's a simply class that store the data in a Dictionary.
+    /// When the program is closed, the data are erased.
+    /// </summary>
+    class UserDataStorageInRAMStrategy : IUserDataStorageStrategy
+    {
+        private readonly IDictionary<string, IUser> users = new Dictionary<string, IUser>();
+
+        public IUser? this[string username] => IsPresent(username) ? this.users[username] : null;
+
+        public ISet<IUser> Users => new HashSet<IUser>(this.users.Values);
+
+        public bool IsPresent(string username) => this.users.ContainsKey(username);
+
+        public void Put(IUser user) => this.users[user.Username] = user;
+    }
+}
