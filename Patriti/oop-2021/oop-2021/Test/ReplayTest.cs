e3512ef572601c0b271daab1e7a849bd1b0a85ca
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2021-04-22 18:42:25 +0200

    Savedreplay in ram Impl

--- Patriti/oop-2021/oop-2021/Test/ReplayTest.cs
+++ Patriti/oop-2021/oop-2021/Test/ReplayTest.cs
@@ -24,5 +24,25 @@ namespace oop_2021
             Assert.Equal("10", replay.MatchID);
         }
 
+
+        [Fact]
+        public void ReplayTest2()
+        {
+            ReplayData replay = new ReplayDataBuilder()
+                .Date(DateTime.Now)
+                .GameType(new GameType())
+                .MatchID("10")
+                .WhiteUser(new User())
+                .BlackUser(new User())
+                .Boards(new List<Board>())
+                .Build();
+
+            ISavedReplay myReplay = new SavedReplayInRam();
+            myReplay.Save(replay);
+            Assert.Equal("10", myReplay.GetSavedReplay("10").MatchID);
+        }
+
+
+
     }
 }
