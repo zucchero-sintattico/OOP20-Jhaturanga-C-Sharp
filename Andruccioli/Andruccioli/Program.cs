220116eca6cd2ad7ae679ef9c527502bd4cb95a0
Manuel Andruccioli <manuandru987@gmail.com>
2021-04-22 23:12:24 +0200

    Create Main

--- Andruccioli/Andruccioli/Program.cs
+++ Andruccioli/Andruccioli/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using OOP20.validator;
+using System;
+using static OOP20.validator.StringValidator.ValidationResult;
 
 namespace OOP20
 {
-    class Program
+    public sealed class Program
     {
-        static void Main()
+        public static void Main()
         {
+            bool loop = true;
+            ILogic logic = new Logic();
 
+            while (loop)
+            {
+                Console.WriteLine("Commands:\n" +
+                    "Login: l\n" +
+                    "Register: r\n" +
+                    "Generate new User: n\n" +
+                    "Print all Users: u\n" +
+                    "Exit: x\n");
+                char choice = Console.ReadKey().KeyChar;
+                Console.Clear();
+                switch (choice)
+                {
+                    case 'l':
+                        logic.Login();
+                        break;
+                    case 'r':
+                        logic.Register();
+                        break;
+                    case 'n':
+                        logic.Generate();
+                        break;
+                    case 'u':
+                        logic.PrintUsers();
+                        break;
+                    case 'x':
+                        loop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Error - Not Implemented Yet");
+                        break;
+                }
+                Console.Clear();
+            }
+
+            Console.WriteLine("Goodby :)");
         }
     }
 }
