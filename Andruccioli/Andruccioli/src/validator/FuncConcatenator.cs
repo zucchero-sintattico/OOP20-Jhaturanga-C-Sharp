bba170bedc07f62f67ee18eeed9d980b3f9ea9be
Manuel Andruccioli <manuandru987@gmail.com>
2021-04-22 20:33:42 +0200

    Create validator and interface

---
+++ Andruccioli/Andruccioli/src/validator/FuncConcatenator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP20.validator
+{
+    /// <summary>
+    /// This interface allow to concatenate Func
+    /// </summary>
+    /// <typeparam name="T">The input</typeparam>
+    /// <typeparam name="TResult">The output</typeparam>
+    public interface IFuncConcatenator<T, TResult>
+    {
+        /// <param name="function">Func to add in the chain</param>
+        /// <returns>a reference to this IFuncConcatenator</returns>
+        IFuncConcatenator<T, TResult> Add(Func<T, TResult> function);
+
+        /// <returns>the chained Func</returns>
+        Func<T, TResult> Create();
+    }
+}
