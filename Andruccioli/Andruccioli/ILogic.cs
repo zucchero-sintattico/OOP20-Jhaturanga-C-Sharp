6ea34c93335ba137528a15e96e35b31005eb130f
Manuel Andruccioli <manuandru987@gmail.com>
2021-04-22 23:11:51 +0200

    Add Logic interface

---
+++ Andruccioli/Andruccioli/ILogic.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP20
+{
+    interface ILogic
+    {
+        /// <summary>
+        /// Login a User
+        /// </summary>
+        void Login();
+
+        /// <summary>
+        /// Register a User
+        /// </summary>
+        void Register();
+
+        /// <summary>
+        /// Print all Users
+        /// </summary>
+        void PrintUsers();
+
+        /// <summary>
+        /// Generate a new user
+        /// </summary>
+        void Generate();
+    }
+}
