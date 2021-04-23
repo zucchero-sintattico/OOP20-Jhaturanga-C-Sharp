83095ffdd4a767692ce6e7612abe02ec17390c71
StefanoScolari <stefano.scolari@studio.unibo.it>
2021-04-23 21:52:05 +0200

    Refactor

--- Scolari/Scolari/Boards/Board.cs
+++ Scolari/Scolari/Boards/Board.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Scolari.Pieces;
+using System.Linq;
 
 namespace Scolari.Boards
 {
@@ -8,20 +9,20 @@ namespace Scolari.Boards
         public int Columns { get; }
         public int Rows { get; }
 
-        private readonly IList<IPiece> piecesOnBoard = new List<IPiece>();
+        private readonly IList<IPiece> _piecesOnBoard;
 
         public Board(ISet<IPiece> startingBoard, int columns, int rows)
         {
             this.Columns = columns;
             this.Rows = rows;
-            this.piecesOnBoard = new List<IPiece>(startingBoard);
+            this._piecesOnBoard = new List<IPiece>(startingBoard);
         }
 
         public bool Add(IPiece pieceToAdd)
         {
             if(this.GetPieceAtPosition(pieceToAdd.PiecePosition) == null)
             {
-                this.piecesOnBoard.Add(pieceToAdd);
+                this._piecesOnBoard.Add(pieceToAdd);
                 return true;
             }
             return false;
@@ -34,38 +35,27 @@ namespace Scolari.Boards
 
         public bool Contains(IPiece pieceToCheck)
         {
-            return this.piecesOnBoard.Contains(pieceToCheck);
+            return this._piecesOnBoard.Contains(pieceToCheck);
         }
 
         public IPiece GetPieceAtPosition(IBoardPosition boardPosition)
         {
-            foreach(IPiece piece in this.piecesOnBoard)
-            {
-                if(piece.PiecePosition.Equals(boardPosition))
-                {
-                    return piece;
-                }
-            }
-            return null;
+            return this._piecesOnBoard.FirstOrDefault(piece => piece.PiecePosition.Equals(boardPosition));
         }
 
         public ISet<IPiece> GetPieces()
         {
-            return new HashSet<IPiece>(this.piecesOnBoard);
+            return new HashSet<IPiece>(this._piecesOnBoard);
         }
 
         public bool Remove(IPiece pieceToRemove)
         {
-            return this.piecesOnBoard.Remove(pieceToRemove);
+            return this._piecesOnBoard.Remove(pieceToRemove);
         }
 
         public bool RemoveAtPosition(IBoardPosition positionToRemove)
         {
-            if(this.GetPieceAtPosition(positionToRemove) != null)
-            {
-                return this.Remove(this.GetPieceAtPosition(positionToRemove));
-            }
-            return false;
+            return this.GetPieceAtPosition(positionToRemove) != null && this.Remove(this.GetPieceAtPosition(positionToRemove));
         }
     }
 }
